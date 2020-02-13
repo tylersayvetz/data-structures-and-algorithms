78569f682841540305d6651853a8b90b03e22901
Tyler <tylersayvetz@gmail.com>
2020-02-12 23:06:30 -0800

    tweak

--- code-challenges/array-binary-search/array-binary-search.test.js
+++ code-challenges/array-binary-search/array-binary-search.test.js
@@ -1,7 +1,9 @@
 let array = [4,8,15,16,23,42, 50];
 
 function binarySearch(arr, target, i = Math.floor(arr.length / 2), iPower = 0, baseI = i) {
-  if (arr[i] < target) {
+  if (i > arr.length || iPower > 500) {
+    return -1;
+  } else if (arr[i] < target - 1) {
     return binarySearch(arr, target, Math.ceil(i + baseI / Math.pow(2,iPower)), ++iPower, baseI);
   } else if (arr[i] > target) {
     return binarySearch(arr, target, Math.floor(baseI / Math.pow(2, iPower)), ++iPower, baseI);
@@ -12,4 +14,21 @@ function binarySearch(arr, target, i = Math.floor(arr.length / 2), iPower = 0, b
   }
 }
 
-console.log(binarySearch(array, 23));
+console.log(binarySearch(array, -111));
+
+
+
+describe('fucntion should do as expected', () => {
+  it('Should respond well to positive inputs', () => {
+    expect(binarySearch([4,8,15,16,23,42, 50],23)).toEqual(4);
+    expect(binarySearch([4,8,15,16,23,42, 50],50)).toEqual(6);
+    expect(binarySearch([4,8,15,16,23,42, 50],4)).toEqual(0);
+  })
+  it('should respond well to bad input', () => {
+    expect(binarySearch([4,8,15,16,23,42, 50],99)).toEqual(-1);
+    expect(binarySearch([4,8,15,16,23,42, 50],0)).toEqual(-1);
+    expect(binarySearch([4,8,15,16,23,42, 50],-111)).toEqual(-1);
+    expect(binarySearch([4,8,15,16,23,42, 50],'a')).toEqual(-1); 
+
+  })
+})
