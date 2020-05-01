4abe18206ae3c451d4ae5502697835ade59e43bf
Tyler <tylersayvetz@gmail.com>
2020-05-01 12:59:49 -0700

    fix symmetricTree

--- code-challenges/interview-prep-post-cf/symmetricTree/symmetricTree.js
+++ code-challenges/interview-prep-post-cf/symmetricTree/symmetricTree.js
@@ -1,29 +1,29 @@
-const isSymmetric = function(root) {
-  const RFQueue = [root]
-  const LFQueue = [root]
-  
-  while(RFQueue.length && LFQueue.length) {
-      const currRight = RFQueue.shift()
-      const currLeft = LFQueue.shift()
-      
-      //if one is null and the other is not. return false.
-      if ((currRight && !currLeft) || (!currRight && currLeft)) {
-          return false
-      //if theyre both nodes.
-      } else if (currRight && currLeft) {
-          // the values are not equal
-          if (currRight.val !== currLeft.val) {
-              return false
-          } else {
-              RFQueue.push(currRight.right)
-              RFQueue.push(currRight.left)
-              
-              LFQueue.push(currLeft.left)
-              LFQueue.push(currLeft.right)
-          }
-      }
-      
-  }
-  return true
-};
+const isSymmetric = function (root) {
+  const RFQueue = [root];
+  const LFQueue = [root];
 
+  while (RFQueue.length && LFQueue.length) {
+    const currRight = RFQueue.shift();
+    const currLeft = LFQueue.shift();
+
+    //if one is null and the other is not. return false.
+    if ((currRight && !currLeft) || (!currRight && currLeft)) {
+      return false;
+      //if theyre both nodes.
+    } else if (currRight && currLeft) {
+      // the values are not equal
+      if (currRight.val !== currLeft.val) {
+        return false;
+      } else {
+        //right queue gets the right child first
+        RFQueue.push(currRight.right);
+        RFQueue.push(currRight.left);
+
+        //left queue gets the left child first.
+        LFQueue.push(currLeft.left);
+        LFQueue.push(currLeft.right);
+      }
+    }
+  }
+  return true;
+};
